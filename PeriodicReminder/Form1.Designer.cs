bd703f9b62d0611745ed571f74f51eb0028e6901
Oktay <okutan.oktay@gmail.com>
2021-12-06 19:25:41 +0100

    UI

--- PeriodicReminder/Form1.Designer.cs
+++ PeriodicReminder/Form1.Designer.cs
@@ -24,13 +24,120 @@ namespace PeriodicReminder {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lboReminders = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblRemindingStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lboReminders
+            // 
+            this.lboReminders.FormattingEnabled = true;
+            this.lboReminders.Items.AddRange(new object[] {
+            "test",
+            "test",
+            "test",
+            "este",
+            "setf",
+            "stes",
+            "test",
+            "test",
+            "test",
+            "este",
+            "setf",
+            "stes"});
+            this.lboReminders.Location = new System.Drawing.Point(129, 12);
+            this.lboReminders.Name = "lboReminders";
+            this.lboReminders.Size = new System.Drawing.Size(120, 95);
+            this.lboReminders.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(13, 13);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(110, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add Reminder";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(13, 43);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(110, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete Reminder";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 116);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(110, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start Reminding";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(12, 174);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(110, 23);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(13, 145);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(110, 23);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Stop Reminding";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // lblRemindingStatus
+            // 
+            this.lblRemindingStatus.AutoSize = true;
+            this.lblRemindingStatus.Location = new System.Drawing.Point(126, 116);
+            this.lblRemindingStatus.Name = "lblRemindingStatus";
+            this.lblRemindingStatus.Size = new System.Drawing.Size(82, 13);
+            this.lblRemindingStatus.TabIndex = 7;
+            this.lblRemindingStatus.Text = "Reminding is off";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(265, 213);
+            this.Controls.Add(this.lblRemindingStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lboReminders);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Periodic Reminder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lboReminders;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblRemindingStatus;
     }
 }
 
